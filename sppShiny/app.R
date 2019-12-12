a474d2edd44639e7198b6decb941994af2a0d26a
Preet Khowaja <pkaa2016@mymail.pomona.edu>
2019-12-12 15:44:18 -0800

    millikan commit

--- sppShiny/app.R
+++ sppShiny/app.R
@@ -1,22 +1,47 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
+# This is a Shiny web application. 
 #
-# Find out more about building applications with Shiny here:
+# It includes visualizations of the stops made in California across time of the day and 
 #
-#    http://shiny.rstudio.com/
-#  Getting started with Shiny
 
 library(shiny)
+library(leaflet)
+library(ggplot2)
 
 # Define UI ----
 ui <- fluidPage(
-    
-)
+  titlePanel("Traffic Stops in Californian Cities"),
+  mainPanel( 
+    #this will create a space for us to display our map
+    leafletOutput(outputId = "mymap"), 
+    #this allows me to put the checkmarks ontop of the map to allow people to view earthquake depth or overlay a heatmap
+    absolutePanel(top = 60, left = 20, 
+                  checkboxInput("markers", "Depth", FALSE),
+                  checkboxInput("heat", "Heatmap", FALSE)
+    )
+  ))
+
 
 # Define server logic ----
-server <- function(input, output) {
-    
+server <- function(input, output, session) {
+  
+  
+  output$mymap <- renderLeaflet({
+    leaflet(data) %>%
+      addPolygons(
+        label=~label, labelOptions = labelOptions(),
+        popup=~popup, popupOptions = popupOptions(),
+        # Shape Options
+        options = pathOptions(),
+        weight = 1, opacity=0.8, color = "#000000",
+        fillColor="#ff0000", fillOpacity=0.7,
+        # Highlighting on mouse-over
+        highlightOptions = highlightOptions(
+          color='#00ff00', weight = 2,
+          opacity = 1, fillOpacity = 1,
+          bringToFront = TRUE, sendToBack = TRUE),
+        group = 'Group-A')
+  })
 }
 
 # Run the app ----
